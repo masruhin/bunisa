00a9e2cbba7ea98b6ceba6f6ce440da29c8f8227
masruhin <masruhin15201029@gmail.com>
2022-11-01 15:14:51 +0700

    revisi

---
+++ admin/index.php
@@ -0,0 +1,307 @@
+<!DOCTYPE html>
+<html class="loading" lang="en" data-textdirection="ltr">
+
+    <!-- BEGIN: Head-->
+    <?php include "head.php"?>
+    <!-- END: Head-->
+
+    <!-- BEGIN: Body-->
+
+    <body
+        class="horizontal-layout horizontal-menu  navbar-floating footer-static  "
+        data-open="hover"
+        data-menu="horizontal-menu"
+        data-col="">
+
+        <!-- BEGIN: Header-->
+      <?php include"header.php"?>
+        <!-- END: Header-->
+
+        <!-- BEGIN: Main Menu-->
+        <div class="horizontal-menu-wrapper">
+            <div
+                class="header-navbar navbar-expand-sm navbar navbar-horizontal floating-nav navbar-light navbar-shadow menu-border container-xxl"
+                role="navigation"
+                data-menu="menu-wrapper"
+                data-menu-type="floating-nav">
+                <div class="navbar-header">
+                    <ul class="nav navbar-nav flex-row">
+                        <li class="nav-item mr-auto">
+                            <a
+                                class="navbar-brand"
+                                href="../vendors/html/ltr/horizontal-menu-template/index.html">
+                                <span class="brand-logo">
+                                    <svg
+                                        viewbox="0 0 139 95"
+                                        version="1.1"
+                                        xmlns="http://www.w3.org/2000/svg"
+                                        xmlns:xlink="http://www.w3.org/1999/xlink"
+                                        height="24">
+                                        <defs>
+                                            <lineargradient
+                                                id="linearGradient-1"
+                                                x1="100%"
+                                                y1="10.5120544%"
+                                                x2="50%"
+                                                y2="89.4879456%">
+                                                <stop stop-color="#000000" offset="0%"></stop>
+                                                <stop stop-color="#FFFFFF" offset="100%"></stop>
+                                            </lineargradient>
+                                            <lineargradient
+                                                id="linearGradient-2"
+                                                x1="64.0437835%"
+                                                y1="46.3276743%"
+                                                x2="37.373316%"
+                                                y2="100%">
+                                                <stop stop-color="#EEEEEE" stop-opacity="0" offset="0%"></stop>
+                                                <stop stop-color="#FFFFFF" offset="100%"></stop>
+                                            </lineargradient>
+                                        </defs>
+                                        <g id="Page-1" stroke="none" stroke-width="1" fill="none" fill-rule="evenodd">
+                                            <g id="Artboard" transform="translate(-400.000000, -178.000000)">
+                                                <g id="Group" transform="translate(400.000000, 178.000000)">
+                                                    <path
+                                                        class="text-primary"
+                                                        id="Path"
+                                                        d="M-5.68434189e-14,2.84217094e-14 L39.1816085,2.84217094e-14 L69.3453773,32.2519224 L101.428699,2.84217094e-14 L138.784583,2.84217094e-14 L138.784199,29.8015838 C137.958931,37.3510206 135.784352,42.5567762 132.260463,45.4188507 C128.736573,48.2809251 112.33867,64.5239941 83.0667527,94.1480575 L56.2750821,94.1480575 L6.71554594,44.4188507 C2.46876683,39.9813776 0.345377275,35.1089553 0.345377275,29.8015838 C0.345377275,24.4942122 0.230251516,14.560351 -5.68434189e-14,2.84217094e-14 Z"
+                                                        style="fill:currentColor"></path>
+                                                    <path
+                                                        id="Path1"
+                                                        d="M69.3453773,32.2519224 L101.428699,1.42108547e-14 L138.784583,1.42108547e-14 L138.784199,29.8015838 C137.958931,37.3510206 135.784352,42.5567762 132.260463,45.4188507 C128.736573,48.2809251 112.33867,64.5239941 83.0667527,94.1480575 L56.2750821,94.1480575 L32.8435758,70.5039241 L69.3453773,32.2519224 Z"
+                                                        fill="url(#linearGradient-1)"
+                                                        opacity="0.2"></path>
+                                                    <polygon
+                                                        id="Path-2"
+                                                        fill="#000000"
+                                                        opacity="0.049999997"
+                                                        points="69.3922914 32.4202615 32.8435758 70.5039241 54.0490008 16.1851325"></polygon>
+                                                    <polygon
+                                                        id="Path-21"
+                                                        fill="#000000"
+                                                        opacity="0.099999994"
+                                                        points="69.3922914 32.4202615 32.8435758 70.5039241 58.3683556 20.7402338"></polygon>
+                                                    <polygon
+                                                        id="Path-3"
+                                                        fill="url(#linearGradient-2)"
+                                                        opacity="0.099999994"
+                                                        points="101.428699 0 83.0667527 94.1480575 130.378721 47.0740288"></polygon>
+                                                </g>
+                                            </g>
+                                        </g>
+                                    </svg>
+                                </span>
+                                <h2 class="brand-text mb-0">SiKersa</h2>
+                            </a>
+                        </li>
+                        <li class="nav-item nav-toggle">
+                            <a class="nav-link modern-nav-toggle pr-0" data-toggle="collapse">
+                                <i
+                                    class="d-block d-xl-none text-primary toggle-icon font-medium-4"
+                                    data-feather="x"></i>
+                            </a>
+                        </li>
+                    </ul>
+                </div>
+                <div class="shadow-bottom"></div>
+                <!-- Horizontal menu content-->
+                <div class="navbar-container main-menu-content" data-menu="menu-container">
+                    <!-- include ../vendors/includes/mixins-->
+                    <ul
+                        class="nav navbar-nav"
+                        id="main-menu-navigation"
+                        data-menu="menu-navigation">
+                        <li class="dropdown nav-item" data-menu="dropdown">
+                            <a
+                                class="dropdown-toggle nav-link d-flex align-items-center"
+                                href="index.html"
+                                data-toggle="dropdown">
+                                <i data-feather="home"></i>
+                                <span data-i18n="Dashboards">Dashboards</span></a>
+                            <ul class="dropdown-menu">
+                                <li class="active" data-menu="">
+                                    <a
+                                        class="dropdown-item d-flex align-items-center"
+                                        href="dashboard-analytics.html"
+                                        data-toggle="dropdown"
+                                        data-i18n="Analytics">
+                                        <i data-feather="activity"></i>
+                                        <span data-i18n="Analytics">Analytics</span></a>
+                                </li>
+                            </ul>
+                        </li>
+                        <li class="dropdown nav-item" data-menu="dropdown">
+                            <a
+                                class="dropdown-toggle nav-link d-flex align-items-center"
+                                href="#"
+                                data-toggle="dropdown">
+                                <i data-feather="box"></i>
+                                <span data-i18n="Misc">Misc</span></a>
+                            <ul class="dropdown-menu">
+                                <li class="dropdown dropdown-submenu" data-menu="dropdown-submenu">
+                                    <a
+                                        class="dropdown-item d-flex align-items-center dropdown-toggle"
+                                        href="#"
+                                        data-toggle="dropdown"
+                                        data-i18n="Menu Levels">
+                                        <i data-feather="menu"></i>
+                                        <span data-i18n="Menu Levels">Menu Levels</span></a>
+                                    <ul class="dropdown-menu">
+                                        <li data-menu="">
+                                            <a
+                                                class="dropdown-item d-flex align-items-center"
+                                                href="#"
+                                                data-toggle="dropdown"
+                                                data-i18n="Second Level">
+                                                <i data-feather="circle"></i>
+                                                <span data-i18n="Second Level">Second Level 2.1</span></a>
+                                        </li>
+                                    </ul>
+                                </li>
+                                <li class="disabled" data-menu="">
+                                    <a
+                                        class="dropdown-item d-flex align-items-center"
+                                        href=""
+                                        data-toggle="dropdown"
+                                        data-i18n="Disabled Menu">
+                                        <i data-feather="eye-off"></i>
+                                        <span data-i18n="Disabled Menu">Disabled Menu</span></a>
+                                </li>
+                                <li data-menu="">
+                                    <a
+                                        class="dropdown-item d-flex align-items-center"
+                                        href="https://pixinvent.com/demo/vuexy-html-bootstrap-admin-template/documentation"
+                                        data-toggle="dropdown"
+                                        data-i18n="Documentation"
+                                        target="_blank">
+                                        <i data-feather="folder"></i>
+                                        <span data-i18n="Documentation">Documentation</span></a>
+                                </li>
+                                <li data-menu="">
+                                    <a
+                                        class="dropdown-item d-flex align-items-center"
+                                        href="https://pixinvent.ticksy.com/"
+                                        data-toggle="dropdown"
+                                        data-i18n="Raise Support"
+                                        target="_blank">
+                                        <i data-feather="life-buoy"></i>
+                                        <span data-i18n="Raise Support">Raise Support</span></a>
+                                </li>
+																
+                            </ul>
+                        </li>
+                    </ul>
+                </div>
+            </div>
+        </div>
+        <!-- END: Main Menu-->
+
+        <!-- BEGIN: Content-->
+        <div class="app-content content ">
+            <div class="content-overlay"></div>
+            <div class="header-navbar-shadow"></div>
+            <div class="content-wrapper container-xxl p-0">
+                <div class="content-header row"></div>
+                <div class="content-body">
+                    <!-- Dashboard Analytics Start -->
+                    <section id="dashboard-analytics">
+                        <div class="row match-height"></div>
+
+                        <div class="row match-height"></div>
+
+                        <div class="row match-height"></div>
+
+                        <!-- List DataTable -->
+                        <div class="row">
+                            <div class="col-12">
+                                <div class="card invoice-list-wrapper">
+                                    <div class="card-datatable table-responsive">
+                                        <table class="invoice-list-table table">
+                                            <thead>
+                                                <tr>
+                                                    <th></th>
+                                                    <th>#</th>
+                                                    <th>
+                                                        <i data-feather="trending-up"></i>
+                                                    </th>
+                                                    <th>Client</th>
+                                                    <th>Total</th>
+                                                    <th class="text-truncate">Issued Date</th>
+                                                    <th>Balance</th>
+                                                    <th>Invoice Status</th>
+                                                    <th class="cell-fit">Actions</th>
+                                                </tr>
+                                            </thead>
+                                        </table>
+                                    </div>
+                                </div>
+                            </div>
+                        </div>
+                        <!--/ List DataTable -->
+                    </section>
+                    <!-- Dashboard Analytics end -->
+
+                </div>
+            </div>
+        </div>
+        <!-- END: Content-->
+
+        <div class="sidenav-overlay"></div>
+        <div class="drag-target"></div>
+
+        <!-- BEGIN: Footer-->
+        <footer class="footer footer-static footer-light">
+            <p class="clearfix mb-0">
+                <span class="float-md-left d-block d-md-inline-block mt-25">COPYRIGHT &copy; 2021<a
+                    class="ml-25"
+                    href="https://1.envato.market/pixinvent_portfolio"
+                    target="_blank">Pixinvent</a>
+                    <span class="d-none d-sm-inline-block">, All rights Reserved</span></span><span class="float-md-right d-none d-md-block">Hand-crafted & Made with<i data-feather="heart"></i></span></p>
+        </footer>
+        <button class="btn btn-primary btn-icon scroll-top" type="button">
+            <i data-feather="arrow-up"></i>
+        </button>
+        <!-- END: Footer-->
+
+        <!-- BEGIN: Vendor JS-->
+        <script src="../vendors/app-assets/vendors/js/vendors.min.js"></script>
+        <!-- BEGIN Vendor JS-->
+
+        <!-- BEGIN: Page Vendor JS-->
+        <script src="../vendors/app-assets/vendors/js/ui/jquery.sticky.js"></script>
+        <script src="../vendors/app-assets/vendors/js/charts/apexcharts.min.js"></script>
+        <script src="../vendors/app-assets/vendors/js/extensions/toastr.min.js"></script>
+        <script src="../vendors/app-assets/vendors/js/extensions/moment.min.js"></script>
+        <script
+            src="../vendors/app-assets/vendors/js/tables/datatable/jquery.dataTables.min.js"></script>
+        <script
+            src="../vendors/app-assets/vendors/js/tables/datatable/datatables.buttons.min.js"></script>
+        <script
+            src="../vendors/app-assets/vendors/js/tables/datatable/datatables.bootstrap4.min.js"></script>
+        <script
+            src="../vendors/app-assets/vendors/js/tables/datatable/dataTables.responsive.min.js"></script>
+        <script
+            src="../vendors/app-assets/vendors/js/tables/datatable/responsive.bootstrap4.min.js"></script>
+        <!-- END: Page Vendor JS-->
+
+        <!-- BEGIN: Theme JS-->
+        <script src="../vendors/app-assets/js/core/app-menu.js"></script>
+        <script src="../vendors/app-assets/js/core/app.js"></script>
+        <!-- END: Theme JS-->
+
+        <!-- BEGIN: Page JS-->
+        <script src="../vendors/app-assets/js/scripts/pages/dashboard-analytics.js"></script>
+        <!-- <script
+        src="../vendors/app-assets/js/scripts/pages/app-invoice-list.js"></script> -->
+        <!-- END: Page JS-->
+
+        <script>
+            $(window).on('load', function () {
+                if (feather) {
+                    feather.replace({width: 14, height: 14});
+                }
+            })
+        </script>
+    </body>
+    <!-- END: Body-->
+
+</html>
